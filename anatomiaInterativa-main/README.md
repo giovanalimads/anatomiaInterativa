3b89f670f07261dad13e411324f8c273dc281895
giovanalimads <gilimaolliveira@gmail.com>
2025-06-27 22:18:51 -0300

    commit

--- anatomiaInterativa-main/README.md
+++ anatomiaInterativa-main/README.md
@@ -2,3 +2,4 @@
 ![image](https://github.com/user-attachments/assets/d8f02c58-9ac5-4ff4-a88c-be085863d8d0)
 
 ![image](https://github.com/user-attachments/assets/5428726a-237c-4fd0-aa26-48696c88d47e)
+
---
+++ anatomiaInterativa-main/js/botoesMenuDireito.js
@@ -0,0 +1,31 @@
+function checar() {
+  const dropZones = document.querySelectorAll('.drop-zone');
+  let acertos = 0;
+
+  dropZones.forEach(zone => {
+    // texto da resposta colocada dentro da drop-zone (supondo que você insira o texto do draggable)
+    const respostaUsuario = zone.textContent.trim().toUpperCase();
+    const respostaCorreta = zone.dataset.resposta.toUpperCase();
+
+    if (respostaUsuario === respostaCorreta) {
+      zone.style.borderColor = 'green';
+      acertos++;
+    } else {
+      zone.style.borderColor = 'red';
+    }
+  });
+
+  alert(`Você acertou ${acertos} de ${dropZones.length} ossos do crânio.`);
+}
+
+function dica() {
+  alert('Dica: Observe a posição dos ossos no crânio e associe o nome que melhor encaixa no local!');
+}
+
+function resetar() {
+  const dropZones = document.querySelectorAll('.drop-zone');
+  dropZones.forEach(zone => {
+    zone.textContent = '';          // limpa o texto dentro da zona
+    zone.style.borderColor = '';   // remove a cor da borda
+  });
+}
